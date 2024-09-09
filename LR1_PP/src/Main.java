91529bce3f5e999344253fa0d1c84d0ae3f9fb98
KirilloXiX <vovk.kiryusha.007@gmail.com>
2024-09-09 21:34:07 +0300

    first version

--- LR1_PP/src/Main.java
+++ LR1_PP/src/Main.java
@@ -1,5 +1,35 @@
+//9.Визначити, які числа серед перших N чисел Фібоначчі можна задати у формі
+       // omega^2 - 1(на 1 менше ніж певний квадрат).
+
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Which number of Fibo");
+    static int fiboMethod(int n) {
+        int a = 1;
+        int b = 1;
+
+
+        return b;
+    }
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int a = 1;
+        int b = 1;
+        int square = 0;
+        int omega = square * square - 1;
+
+        System.out.println("Which number of Fibos");
+        int fibo = myObj.nextInt();
+        for (int i = 3; i <= fibo; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+            for (int j = 1; j < 1000; j++) {
+                if (b == j * j - 1) {
+                    System.out.println(b + "(" + j + "*" + j + "-1)");
+                }
+            }
+
+        }
     }
 }
