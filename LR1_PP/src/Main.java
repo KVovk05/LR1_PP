74bf0ddcf94f6397e23d9da6c13339e108c3b78a
KirilloXiX <vovk.kiryusha.007@gmail.com>
2024-09-10 20:45:15 +0300

    updateomega checking method

--- LR1_PP/src/Main.java
+++ LR1_PP/src/Main.java
@@ -1,22 +1,13 @@
-//9.Визначити, які числа серед перших N чисел Фібоначчі можна задати у формі
-       // omega^2 - 1(на 1 менше ніж певний квадрат).
 
 import java.util.Scanner;
 public class Main {
-    static int fiboMethod(int n) {
-        int a = 1;
-        int b = 1;
 
-
-        return b;
-    }
 
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         int a = 1;
         int b = 1;
-        int square = 0;
-        int omega = square * square - 1;
+
 
         System.out.println("Which number of Fibos");
         int fibo = myObj.nextInt();
@@ -24,12 +15,10 @@ public class Main {
             int c = a + b;
             a = b;
             b = c;
-            for (int j = 1; j < 1000; j++) {
-                if (b == j * j - 1) {
-                    System.out.println(b + "(" + j + "*" + j + "-1)");
-                }
+            double res = Math.sqrt(b + 1);
+            if((int)res == res){
+                System.out.println(b + "(" + (int)res + "*" + (int)res + "-1)");
             }
-
         }
     }
 }
